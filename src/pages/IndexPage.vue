39e5b5dc6ccf99138e69f90f55ec3d0b1c40352e
Joel <Joel.innes@gmail.com>
2020-03-04 16:59:15 +1300

    re added HomeCard rendering to home page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,38 @@
 <template>
-  <h1>Home Page</h1>
+  <div class="">
+    <div v-for="card in cards" :key="card.name">
+      <HomeCard v-bind:userData="card"/>
+    </div>
+  </div>
 </template>
 
 <script>
+import HomeCard from "../components/home/HomeCard"
 export default {
 name: 'IndexPage',
     components: {
-
+      HomeCard
+    },
+    data: function() {
+      return {
+        cards: [
+          {
+            name: "John",
+            imageUrl: "https://image.shutterstock.com/image-photo/portrait-smiling-red-haired-millennial-260nw-1194497251.jpg",
+            role: "Web Developer"
+          },
+          {
+            name: "John",
+            imageUrl: "https://image.shutterstock.com/image-photo/portrait-smiling-red-haired-millennial-260nw-1194497251.jpg",
+            role: "Web Developer"
+          },
+          {
+            name: "John",
+            imageUrl: "https://image.shutterstock.com/image-photo/portrait-smiling-red-haired-millennial-260nw-1194497251.jpg",
+            role: "Web Developer"
+          }
+        ]
+      }
     }
 }
 </script>
